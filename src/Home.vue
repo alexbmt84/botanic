bc7a0054b9e4fd18e45b56bbe304c3bc4c5a3f74
Alexis Beaumont <alexisbeaumont97@gmail.com>
2024-04-10 23:51:38 +0200

    Modifs after production deployment

--- src/Home.vue
+++ src/Home.vue
@@ -174,7 +174,7 @@
 
         <div class="mb-3 pt-0 mx-auto">
 
-          <input class="btn w-1/3 cursor-pointer mx-auto bg-[#475F45] hover:bg-[#475F45]/80 duration-300 transition-colors border-2 border-[#475F45] px-6 block text-center py-3 uppercase text-sm font-bold leading-4 tracking-widest text-white mb-5"
+          <input class="btn lg:w-1/3 cursor-pointer mx-auto bg-[#475F45] hover:bg-[#475F45]/80 duration-300 transition-colors border-2 border-[#475F45] px-6 block text-center py-3 uppercase text-sm font-bold leading-4 tracking-widest text-white mb-5"
                 type="submit" 
                 value="Envoyer">
 
---
+++ src/VueRecaptcha.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <form @submit.prevent="submitForm">
+        <label>
+          Name:
+          <input type="text" v-model="name">
+        </label>
+        <label>
+          Email:
+          <input type="email" v-model="email">
+        </label>
+        <vue-recaptcha ref="recaptcha" @verify="onVerify"></vue-recaptcha>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </template>
+  <script>
+    import Vue from 'vue'
+    import VueRecaptcha from 'vue-recaptcha'
+    
+    Vue.component('vue-recaptcha', VueRecaptcha)
+    
+    export default {
+      data() {
+        return {
+          name: '',
+          email: ''
+        }
+      },
+      methods: {
+        submitForm() {
+          this.$refs.recaptcha.execute()
+        },
+        onVerify(response) {
+          // Verify the response with your server and submit the form
+          console.log(response)
+        }
+      }
+    }
+  </script>
+
